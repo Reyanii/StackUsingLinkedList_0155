e88b3aa717b0a1d0c9dacd3cfed0e84fb3dc6480
Reyanii <indriyaniv9@gmail.com>
2025-05-22 11:01:20 +0800

    ++NewNode

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -24,4 +24,11 @@ public:
  }
 
  //Push opration: insert an element onto the top of the stack
+int push(int value) {
+  Node* newNode = new Node(); //1. Allocate memory for the new node
+  newNode-> data = value; //2. assign value
+  newNode->next = top; //3. Set the next pointer of the new node to the current top
+  top = newNode; //4. Update the top pointer to new node
+  cout << "Push value: " << value << endl;
 }
+};

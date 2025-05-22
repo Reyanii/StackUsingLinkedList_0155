5acc6e886334aeb283e76180c1246c0384c20de3
Reyanii <indriyaniv9@gmail.com>
2025-05-22 11:40:54 +0800

    end

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -121,6 +121,13 @@ int main()
             break;
 
         case 4:
+            cout << "Exiting program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Try again." endl;
+            break;
         }
+        cout << endl;
     }
+    return 0;
 }

f74168e4f96941f10352195b69f3759024af161c
Reyanii <indriyaniv9@gmail.com>
2025-05-22 11:56:05 +0800

    menambahkan delete tamp

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -49,6 +49,7 @@ public:
         Node *temp = top; // create a temporary pointer to the top node
         top = top->next;  // Update the top pointer to the next node
         cout << "Popped value: " << top->data << endl;
+        delete tamp;
     }
 
     // Peek/Top operation: Retrive the value of the

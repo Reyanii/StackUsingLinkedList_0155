d70ecf268092fbc20b5a5530949cef3fb8eacde3
Reyanii <indriyaniv9@gmail.com>
2025-05-22 11:31:18 +0800

    case 2

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -100,6 +100,10 @@ int main()
             stack.push(value);
             break;
         case 2
+            if(!stack.isEmpty())
+            {
+                stack.pop();
+            }
         }
     }
 }
